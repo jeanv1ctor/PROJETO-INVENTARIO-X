696164671c16e34792dd7f12e779d1e4ee21364b
unknown <jean2563victor@gmail.com>
2023-12-26 14:56:52 -0300

    adicionando data annotations no model user

--- Api_Inventario/Models/UserModel.cs
+++ Api_Inventario/Models/UserModel.cs
@@ -1,10 +1,23 @@
-﻿namespace Api_Inventario;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api_Inventario;
 
 public class UserModel
 {
+    [Key]
     public int Id { get; set; }
-    public string Nome { get; set; }    
+
+    [Required]
+    [MinLength(5, ErrorMessage = "Quantidade minima de caracteres para o nome do usuario = 5")]
+    [MaxLength(30, ErrorMessage = "Quantidade maxima de caracteres  para o nome do usuario  = 30")]
+    public string Nome { get; set; }
+
+    [Required]
+    [MinLength(10, ErrorMessage = "Quantidade minima de caracteres para email = 5")]
+    [MaxLength(100, ErrorMessage = "Quantidade maxima de caracteres  para email = 100")]
     public string Email { get; set; }
+
+    [Required]
     public string Password { get; set; }
 
     public DateTime CreatedAt { get; set; }
